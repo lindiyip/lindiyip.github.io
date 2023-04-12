539da58abaf9e835c038d12ef59f258ebabbe30c
lindiyip <lindiyip1997@hotmail.com>
2023-04-13 02:13:38 +0800

    main frame

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("town").classList.toggle("show");
+  document.getElementById("flatModel").classList.toggle("show");
+  document.getElementById("lease").classList.toggle("show");
+}
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
