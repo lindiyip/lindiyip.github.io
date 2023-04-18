d161e94f83903944a1814967f5f5600685b687b5
lindiyip <lindiyip1997@hotmail.com>
2023-04-19 00:48:24 +0800

    dropdown selection completed

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 var lookup_town = {};
-var result_town = [];
+var dropdown_town = [];
 
 var lookup_type = {};
-var result_type = [];
+var dropdown_type = [];
 
 var records = {};
+
+// call API
+$.ajax({
+  url: "https://data.gov.sg/api/action/datastore_search",
+  type: "GET",
+  data: { resource_id: "1b702208-44bf-4829-b620-4615ee19b57c" },
+  dataType: "JSON",
+  success: function (data) {
+    // console.log(data["result"]);
+    var records = data["result"]["records"];
+    // console.log(records);
+
+    // create droplist_town
+    var items_town = records;
+    for (let item, i = 0; (item = items_town[i++]); ) {
+      let name = item.town;
+
+      if (!(name in lookup_town)) {
+        lookup_town[name] = 1;
+        dropdown_town.push(name);
+      }
+    }
+    dropdown_town.sort();
+    $(function () {
+      console.log(dropdown_town);
+      $.each(dropdown_town, function (i, option) {
+        $("#selTown").append($("<option/>").attr("value", option).text(option));
+      });
+    });
+
+    // create droplist_type
+    var items_type = records;
+    for (let item, i = 0; (item = items_type[i++]); ) {
+      let name = item.flat_type;
+
+      if (!(name in lookup_type)) {
+        lookup_type[name] = 1;
+        dropdown_type.push(name);
+      }
+    }
+    dropdown_type.sort();
+    // console.log(dropdown_type);
+    $(function () {
+      console.log(dropdown_type);
+      $.each(dropdown_type, function (i, option) {
+        $("#selType").append($("<option/>").attr("value", option).text(option));
+      });
+    });
+  },
+});
 
 // fetch data button
 function fetch() {
@@ -17,72 +67,17 @@ function fetch() {
       console.log(data["result"]);
       var records = data["result"]["records"];
       console.log(records);
-
-      let tbodyEl = $("#targetElement"); // HTML Element where we will render data into
-      records.forEach((r) => {
-        tbodyEl.append("<tr>");
-        tbodyEl.append(`<td>${r.town}</td>`);
-        // tbodyEl.append(`<td> </td>`);
-        tbodyEl.append(`<td>${r.lease_commence_date}</td>`);
-        // tbodyEl.append(`<td> </td>`);
-        tbodyEl.append(`<td>${r.flat_type}</td>`);
-        // tbodyEl.append(`<td> </td>`);
-        tbodyEl.append(`<td>${r.resale_price}</td>`);
-        tbodyEl.append("</tr>");
-      });
-
-      // create droplist_town
-      var items_town = records;
-      for (let item, i = 0; (item = items_town[i++]); ) {
-        let name = item.town;
-
-        if (!(name in lookup_town)) {
-          lookup_town[name] = 1;
-          result_town.push(name);
-        }
-      }
-      result_town.sort();
-      console.log(result_town);
-
-      // create droplist_type
-      var items_type = records;
-      for (let item, i = 0; (item = items_type[i++]); ) {
-        let name = item.flat_type;
-
-        if (!(name in lookup_type)) {
-          lookup_type[name] = 1;
-          result_type.push(name);
-        }
-      }
-      result_type.sort();
-      console.log(result_type);
     },
   });
-}
 
-// toggle between hiding and showing the dropdown content */
-function dropdownTown(e) {
-  document.getElementById("town").classList.toggle("show");
-}
+  // User dropdown selection
+  var townSelection = document.getElementById("selTown").value;
+  document.getElementById("demo1").innerHTML = townSelection;
+  console.log(townSelection);
 
-// function dropdownTown(e) {
-//   let select = document.createElement("select");
-//   select.name = "town";
-//   select.id = "town";
-
-//   for (const val of result_town) {
-//     let option = document.createElement("option");
-//     option.value = val;
-//     select.appendChild(option);
-//   }
-//   document.getElementById("town").appendChild(select);
-// }
-
-function dropdownFlat(e) {
-  document.getElementById("flatModel").classList.toggle("show");
-}
-function dropdownLease(e) {
-  document.getElementById("lease").classList.toggle("show");
+  var typeSelection = document.getElementById("selType").value;
+  document.getElementById("demo2").innerHTML = typeSelection;
+  console.log(typeSelection);
 }
 
 // Close the dropdown menu if the user clicks outside of it
